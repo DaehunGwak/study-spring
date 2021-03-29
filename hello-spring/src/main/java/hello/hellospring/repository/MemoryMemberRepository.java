43d0ed128917404cfd233e8fdbe16b711a24bf65
DaehunGwak <rhkrgns56@naver.com>
2021-03-30 00:40:30 +0900

    Add beans using annotation

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>(); // 지금은 동시성 문제를 고려하지 않
