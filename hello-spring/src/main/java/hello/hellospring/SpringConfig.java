e5b5331d1e2a44f8cca2a22662a69649a05e96c0
DaehunGwak <rhkrgns56@naver.com>
2021-04-03 08:22:11 +0900

    Add SpringDataJpaMemberRepository

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -13,25 +13,28 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
     private DataSource dataSource;
     private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em) {
+    public SpringConfig(MemberRepository memberRepository, DataSource dataSource, EntityManager em) {
+        this.memberRepository = memberRepository;
         this.dataSource = dataSource;
         this.em = em;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
