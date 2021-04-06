fc08196424ee9863a348bc34d0b992e06795229d
DaehunGwak <rhkrgns56@naver.com>
2021-04-06 22:14:05 +0900

    Add lombok test, templates, devtools, template controller

--- practical-boot-jpa-1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ practical-boot-jpa-1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
