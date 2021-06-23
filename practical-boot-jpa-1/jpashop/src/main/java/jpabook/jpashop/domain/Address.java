3a8b178ea8e26e4e0ea1c32b114ef30ee314e912
DaehunGwak <rhkrgns56@naver.com>
2021-06-23 23:44:51 +0900

    jpa 활용 1 - 도메인 분석 설계

---
+++ practical-boot-jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
