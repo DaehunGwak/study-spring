e6daae53254607b194ac858970aae2f7ca068765
DaehunGwak <rhkrgns56@naver.com>
2022-02-19 10:19:37 +0900

    feat: 7. html, css, js 추가

---
+++ inflearn/jpashop/src/test/java/io/ordi/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,30 @@
+package io.ordi.jpabook.jpashop.service;
+
+import io.ordi.jpabook.jpashop.domain.item.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() throws Exception {
+        Book book = em.find(Book.class, 1L);
+
+        // TX
+        book.setName("asdfa");
+
+        // 변경 감지
+    }
+}
