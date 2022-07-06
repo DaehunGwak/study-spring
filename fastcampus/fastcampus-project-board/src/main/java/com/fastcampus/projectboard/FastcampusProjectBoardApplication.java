6000fc7e12a1e93ff648087a96a4ae7021c9cb9b
DaehunGwak <rhkrgns56@naver.com>
2022-07-06 23:46:48 +0900

    fastcampus 스프링 게시판 프로젝트 초기화

---
+++ fastcampus/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/FastcampusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusProjectBoardApplication.class, args);
+    }
+
+}
