736a82e3a15bc188ce2c0bf97bc612a02fbe3ef4
DaehunGwak <rhkrgns56@naver.com>
2022-09-11 23:58:39 +0900

    init project-sns

---
+++ fastcampus/project-sns/src/main/java/io/ordi/projectsns/ProjectSnsApplication.java
@@ -0,0 +1,13 @@
+package io.ordi.projectsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSnsApplication.class, args);
+    }
+
+}
